46962cf5b9f3caf06183cd66db420f35d3df008e
jubeo <jubeo@jubeo-K43SJ.(none)>
2014-02-05 12:23:41 +0100

    solu 02 stupid coaching

--- 02-Stupid-coaching/app/controllers/welcome.rb
+++ 02-Stupid-coaching/app/controllers/welcome.rb
@@ -1,15 +1,25 @@
 get '/' do
   @coach_message = params[:coach_message] || "what do you want kid ?"
   # Look in app/views/index.erb
-  
-  puts "self : #{self}"
-  puts "self class: #{self.class}"
-  puts "self instance variables from GET '/' route handler: #{self.instance_variables}"
   erb :index
 end
 
 post '/coach' do
-  puts "self instance variable from POST '/coach' route handler: #{self.instance_variables}"
   
-  "Implement your coach response ! params => #{params.inspect}"
+  def stupid_coaching(answer)
+  		if answer == "idiot"
+  			@coach_message = "méchant Dobby !" 
+  			erb :index
+  		elsif answer == "bonjour"
+  			@coach_message = "BYE BYE !!!"
+				erb :exit
+  		else
+  			@coach_message = "Je ne comprends pas. Essaye encore"
+  			erb :index
+  		end
+  	
+  end
+
+ stupid_coaching(params[:what])
+
 end
