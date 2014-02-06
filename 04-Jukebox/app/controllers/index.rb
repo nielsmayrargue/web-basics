0a4fe7e1c8cbf7e6ddf5493553fa67aeedc2a885
jubeo <jubeo@jubeo-K43SJ.(none)>
2014-02-06 16:26:39 +0100

    solu jukebox

--- 04-Jukebox/app/controllers/index.rb
+++ 04-Jukebox/app/controllers/index.rb
@@ -1,8 +1,10 @@
 get '/' do
+
   erb :index
 end
 
 get '/new' do
+	@album = Album.new
   erb :form
 end
 
@@ -10,10 +12,29 @@ post '/' do
   # Create a new album and add it to the $albums array 
   # You should use only one parameter of the params hash named :album
   # You should use only Album#new and Album#save
+  album = Album.new(params[:album])
+  album.save
+
   erb :index
 end
 
-get '/:id' do 
-  # Find the album with index :id and transmit it to the show view
-  erb :show
+get '/:id' do
+@album = Album.find(params[:id].to_i)
+erb :show
+end
+
+get '/:id/delete' do
+Album.delete(params[:id].to_i)
+erb :index
+end
+
+get '/:id/edit' do
+	@id = params[:id].to_i
+	@album = $albums[@id]
+	erb :form
+end
+
+post '/:id' do
+  $albums[params[:id].to_i] = Album.new(params[:album])
+  erb :index
 end
--- 04-Jukebox/app/models/album.rb
+++ 04-Jukebox/app/models/album.rb
@@ -2,8 +2,20 @@ $albums = []
 
 class Album
   
-  attr_reader :name, :artist, :year
+  attr_reader :name, :artist, :year, :list
   
+  def self.list
+    if $albums.empty? 
+      "There are no albums yet"
+    else  
+      @list = []
+      $albums.each do |album|
+        list << album
+      end
+      @list.join(" ")
+    end
+  end
+
   def initialize(args = {})
     @name = args[:name]
     @artist = args[:artist]
@@ -22,14 +34,15 @@ class Album
     @name = args[:name]
     @artist = args[:artist]
     @year = args[:year]
+    
   end
   
-  def delete
-    $albums.delete(self)
+  def self.delete(index)
+    $albums.delete_at(index)
   end
-  
+
   def to_s
-    "#{@name} (#{@year}) by #{@artist}"
+    "#{@name} (#{@year}) by #{@artist}\n"
   end
     
 end
